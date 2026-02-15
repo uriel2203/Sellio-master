6062eb3680a0cac3a9c44ed993ab34f068c4c010
uriel2203 <urieljohnchavez@gmail.com>
2026-02-16 03:01:08 +0800

    Fix storage and config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,12 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
-    /**
-     * Set the storage path for Vercel's read-only filesystem.
-     * We use /tmp because it is the only writable directory on Vercel.
-     */
-    ->useStoragePath(env('APP_ENV') === 'production' ? '/tmp' : null)
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -21,3 +16,13 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+/**
+ * Set the storage path for Vercel's read-only filesystem.
+ * We use /tmp because it is the only writable directory on Vercel.
+ */
+if (env('APP_ENV') === 'production') {
+    $app->useStoragePath('/tmp');
+}
+
+return $app;
